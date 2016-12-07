e53c99cbf59822c4960ed5ec231e2dd63cb0986d
Gennaro Avolio-Toly <gennaro.avolio-toly@smashingideas.com>
2016-12-06 17:27:22 -0800

    day 3 part 2

--- three.rb
+++ three.rb
@@ -5,9 +5,9 @@ def triangle?(sides)
   (sides[0] + sides[1]) > sides[2]
 end
 
-triangles.map! do |triangle|
+def check_sides?(triangle)
   status = true
-  triangle.size.times do
+  3.times do
     status = triangle?(triangle)
     break unless status
     triangle.rotate!(1)
@@ -15,4 +15,13 @@ triangles.map! do |triangle|
   status
 end
 
-p triangles.count(true)
+p triangles.map { |triangle| check_sides?(triangle) }.count(true)
+
+# part 2
+triangles_2 = File.readlines('three.txt')
+                  .map { |set| set.split(' ').map(&:strip).map(&:to_i) }
+                  .each_slice(3).to_a
+
+p triangles_2 .flat_map { |set| set[0].zip(set[1], set[2]) }
+              .map! { |triangle| check_sides?(triangle) }
+  .count(true)
